3732a43469c598a38c39c37bb14e958b81d58218
lubelle <belleliullh@gmail.com>
2019-04-24 15:26:18 -0400

    ex 2 - post

---
+++ Post.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace PropertyDemo
+{
+    public class Post
+    {
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public DateTime Created { get; set; }
+        public int Vote { get; private set; }
+
+        public Post(int vote)
+        {
+            Vote = vote;
+        }
+        public int UpVote()
+        {
+            return Vote += 1;
+        }
+        public int DownVote()
+        {
+            if (Vote >= 1)
+                Vote -= 1;
+
+            return Vote;
+        }
+    }
+}
